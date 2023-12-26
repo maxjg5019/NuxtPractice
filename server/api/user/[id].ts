aa8138081231fa8b7ded04b88ca4b5c4fcdedf40
maxjg5019 <maxjg5019@gmail.com>
2023-12-27 00:40:35 +0800

    刪除 開發初期測試相關Code & 用三元素刻出來的答辯

--- server/api/user/[id].ts
+++
@@ -1,6 +0,0 @@
-import users from '@/public/user.json';
-
-export default defineEventHandler(event => {
-    const id:string | undefined = getRouterParam(event, 'id');
-    return users.find(user => user.id === parseInt(id || '0') || {});
-});
