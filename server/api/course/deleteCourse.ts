80435c47f3b060ca9c045d848014805ce6780475
maxjg5019 <maxjg5019@gmail.com>
2024-01-02 01:16:34 +0800

    fix: 刪除課程時要同步刪掉學生修課清單的課號

--- server/api/course/deleteCourse.ts
+++ server/api/course/deleteCourse.ts
@@ -1,5 +1,6 @@
 import { MongoClient, ObjectId } from 'mongodb';
 import type { course } from '~/server/models/courseModel';
+import type { user } from '~/server/models/userModel';
 
 const config = useRuntimeConfig();
 const client = new MongoClient(config.MONGODB_URI);
@@ -11,14 +12,23 @@ export default defineEventHandler(async (event) => {
 
   try {
     const db = client.db(dbName);
-    const collection = db.collection<course>('course');
-    const result = await collection.deleteOne({ _id: new ObjectId(courseId) });
+    const courseCollection = db.collection<course>('course');
+    const userCollection = db.collection<user>('user');
+    const deleteResult  = await courseCollection.deleteOne({ _id: new ObjectId(courseId) });
 
-    console.log('result', result);
-    if (result.deletedCount === 1) {
-      return { success: true, message: '刪除成功' };
+    if (deleteResult .deletedCount === 0) {
+      return { success: false, message: '未找到課程' };
+    }
+
+    const updateResult = await userCollection.updateMany(
+      { courseList: new ObjectId(courseId) },
+      { $pull: { courseList: new ObjectId(courseId) } }
+    );
+
+    if (updateResult.modifiedCount > 0) {
+      return { success: true, message: '課程刪除成功，用戶修課列表已更新' };
     } else {
-      return { success: false, message: '未找到相符課程' };
+      return { success: false, message: '課程刪除成功，但無需更新用戶修課列表' };
     }
   } catch (err) {
     return { success: false, message: 'DB連線失敗' };
